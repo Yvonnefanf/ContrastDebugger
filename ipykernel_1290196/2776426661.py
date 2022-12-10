0dd05f79c4a12bc1af4344ca0844f882e4106253
Yvonnefanf <m18522608197@163.com>
2022-12-10 21:24:00 +0800

    fix

--- ipykernel_1290196/2776426661.py
+++
@@ -1,8 +0,0 @@
-# cca = CCA(n_components=1)
-# # 训练数据
-# cca.fit(X, Y2)
-# # print(X)
-# X_train_r, Y_train_r = cca.transform(X, Y2)
-# # print(X_train_r)
-
-# print(np.corrcoef(X_train_r[:, 0], Y_train_r[:, 0])[0, 1]) #输出相关系数

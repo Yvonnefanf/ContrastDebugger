5215d176de277384587fd8d989f5054da9861fb3
Yvonnefanf <m18522608197@163.com>
2023-01-07 18:31:11 +0800

    fix

--- representationTrans/eval/evaluator.py
+++ representationTrans/eval/evaluator.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 import os
 import json
+import numpy as np
+from representationTrans.eval.evaluate import *
 
 
 class EvaluatorAbstractClass(ABC):
@@ -9,31 +11,43 @@ class EvaluatorAbstractClass(ABC):
         self.projector = projector
 
     @abstractmethod
-    def evaluate_conf(labels, ori_pred, new_pred):
+    def evaluate_confidence(labels, ori_pred, new_pred):
         pass
 
 
 class Evaluator(EvaluatorAbstractClass):
-    def __init__(self, data_provider, projector, *args, **kwargs):
+    def __init__(self, data_provider, projector, verbose=1,*args, **kwargs):
         self.data_provider = data_provider
         self.projector = projector
+        self.verbose =verbose
     
-    def evaluate_confidence(labels, ori_pred, new_pred):
-    """
-    the confidence difference between original data and reconstruction data
-    :param labels: ndarray, shape(N,), the original prediction for each point
-    :param ori_pred: ndarray, shape(N,10), the prediction of original data
-    :param new_pred: ndarray, shape(N,10), the prediction of reconstruction data
-    :return diff: float, the mean of confidence difference for each point
-    """
-    old_conf = [ori_pred[i, labels[i]] for i in range(len(labels))]
-    new_conf = [new_pred[i, labels[i]] for i in range(len(labels))]
-    old_conf = np.array(old_conf)
-    new_conf = np.array(new_conf)
+    def evaluate_confidence(self, labels, ori_pred, new_pred):
+        """
+        the confidence difference between original data and reconstruction data
+        :param labels: ndarray, shape(N,), the original prediction for each point
+        :param ori_pred: ndarray, shape(N,10), the prediction of original data
+        :param new_pred: ndarray, shape(N,10), the prediction of reconstruction data
+        :return diff: float, the mean of confidence difference for each point
+        """
+        old_conf = [ori_pred[i, labels[i]] for i in range(len(labels))]
+        new_conf = [new_pred[i, labels[i]] for i in range(len(labels))]
+        old_conf = np.array(old_conf)
+        new_conf = np.array(new_conf)
+    
+        diff = np.abs(old_conf - new_conf)
+        # return diff.mean(), diff.max(), diff.min()
+        return diff.mean()
+    
+    def eval_nn_test(self, epoch, train_data, embedding, n_neighbors):
+        # train_data = self.data_provider.train_representation(epoch)
+        # test_data = self.data_provider.test_representation(epoch)
+        # fitting_data = np.concatenate((train_data, test_data), axis=0)
+        # embedding = self.projector.batch_project(epoch, train_data)
+        val = evaluate_proj_nn_perseverance_knn(train_data, embedding, n_neighbors=n_neighbors, metric="euclidean")
+       
+        print("#test# nn preserving : {:.2f}/{:d} in epoch {:d}".format(val, n_neighbors, epoch))
+        return val
 
-    diff = np.abs(old_conf - new_conf)
-    # return diff.mean(), diff.max(), diff.min()
-    return diff.mean()
 
     def batch_inv_preserve(self, epoch, data):
         """
